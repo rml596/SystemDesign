cdab58035be241a9f4bdad8bdcbb0251a5a1d841
Robert Lynch <rml596@gmail.com>
2017-01-19 13:17:02 -0500

    Added third label, change colors

--- LynchSwapLabels/src/LynchSwapLabelsFrame.java
+++ LynchSwapLabels/src/LynchSwapLabelsFrame.java
@@ -14,10 +14,12 @@ import java.awt.event.ActionEvent;
 public class LynchSwapLabelsFrame extends JFrame {
 
 	private JPanel contentPane;
-	private final JLabel lblHello = new JLabel("Hello");
-	private final JLabel lblGoodbye = new JLabel("Goodbye");
+	private final JLabel lbltxt1 = new JLabel("Hello");
+	private final JLabel lbltxt2 = new JLabel("Goodbye");
 	private final JButton btnSwap = new JButton("Swap");
 	private final JButton btnHide = new JButton("Hide");
+	private final JLabel lbltxt3 = new JLabel("Don't Go");
+	private final JButton btnColor = new JButton("Color");
 
 	/**
 	 * Launch the application.
@@ -49,16 +51,16 @@ public class LynchSwapLabelsFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		lblHello.setBounds(147, 22, 130, 29);
-		lblHello.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
-		lblHello.setForeground(Color.ORANGE);
+		lbltxt1.setBounds(147, 22, 130, 29);
+		lbltxt1.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+		lbltxt1.setForeground(Color.ORANGE);
 		
-		contentPane.add(lblHello);
-		lblGoodbye.setForeground(Color.BLUE);
-		lblGoodbye.setFont(new Font("Stencil", Font.PLAIN, 16));
-		lblGoodbye.setBounds(147, 124, 90, 29);
+		contentPane.add(lbltxt1);
+		lbltxt2.setForeground(Color.BLUE);
+		lbltxt2.setFont(new Font("Stencil", Font.PLAIN, 16));
+		lbltxt2.setBounds(147, 63, 90, 29);
 		
-		contentPane.add(lblGoodbye);
+		contentPane.add(lbltxt2);
 		btnSwap.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				do_btnSwap_actionPerformed(arg0);
@@ -75,16 +77,43 @@ public class LynchSwapLabelsFrame extends JFrame {
 		btnHide.setBounds(173, 195, 89, 23);
 		
 		contentPane.add(btnHide);
+		lbltxt3.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		lbltxt3.setBounds(157, 104, 61, 16);
+		
+		contentPane.add(lbltxt3);
+		btnColor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_btnColor_actionPerformed(e);
+			}
+		});
+		btnColor.setBounds(160, 222, 117, 29);
+		
+		contentPane.add(btnColor);
 	}
 	protected void do_btnSwap_actionPerformed(ActionEvent arg0) {
 		
-		String hello =  lblHello.getText();
-		lblHello.setText(lblGoodbye.getText());
-		lblGoodbye.setText(hello);
+		String txt1 =  lbltxt1.getText();
+		String txt2 = lbltxt2.getText();
+		lbltxt1.setText(lbltxt3.getText());
+		lbltxt2.setText(txt1);
+		lbltxt3.setText(txt2);
 	}
 	protected void do_btnHide_actionPerformed(ActionEvent arg0) {
+		if(lbltxt1.isVisible()){
+			lbltxt1.setVisible(!lbltxt1.isVisible());
+			btnHide.setText("Show");
+		}
+		else{
+			lbltxt1.setVisible(!lbltxt1.isVisible());
+			btnHide.setText("Hide");
+		}
 		
-		lblHello.setVisible(!lblHello.isVisible());
-		
+	}
+	protected void do_btnColor_actionPerformed(ActionEvent e) {
+		Color color1 = lbltxt1.getForeground();
+		Color color2 = lbltxt2.getForeground();
+		lbltxt1.setForeground(lbltxt3.getForeground());
+		lbltxt2.setForeground(color1);
+		lbltxt3.setForeground(color2);
 	}
 }
